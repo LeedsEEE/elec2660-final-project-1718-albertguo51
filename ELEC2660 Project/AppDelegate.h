cc8802cbd64dae4dcfe83011b62d2bfa00ae6cd8
Jialeng <albertguo51@gmail.com>
2017-11-07 16:39:15 +0000

    Initial Commit

---
+++ ELEC2660 Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ELEC2660 Project
+//
+//  Created by Jialeng on 11/7/17.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ELEC2660 Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ELEC2660 Project
+//
+//  Created by Jialeng on 11/7/17.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
